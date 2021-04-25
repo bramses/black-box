9d4730fd5a65fdab34961115de64640a80672eb1
Bram Adams <3282661+bramses@users.noreply.github.com>
2021-04-25 19:53:01 -0400

    changelist: - defn upvote downvote - hr under grids - map and sort works correctly - app css cleanup

--- v2/src/App.js
+++ v2/src/App.js
@@ -1,26 +1,31 @@
-// import './App.css';
-// import './Ethereum';
 import Definition from './Definition';
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
 
+
+  // users -> from json
+
   const tommy = { address: '0x123' }
   const becca = { address: '0x456' }
+
+
+  // word.js
 
   const [word, setWord] = useState({
     name: "fish",
     bounty: '0.00003',
     definitions: [
       {
-        name: "ocean animal with fins",
-        score: 8,
-        user: becca
-      },
-      {
         name: "waterboi",
         score: 6,
         user: tommy
+      },
+      {
+        name: "ocean animal with fins",
+        score: 8,
+        user: becca
       }
     ]
   })
@@ -38,24 +43,44 @@ function App() {
       }
     }
 
-    newDefinitions = newDefinitions.sort((a, b) => {
-      return b.score - a.score
-    })
-
-    console.log(newDefinitions)
-
-    setWord({ definitions: newDefinitions , ...word})
+    setWord({ definitions: [...newDefinitions], ...word })
+    changeBounty()
   }
 
-  useEffect(() => {
-    console.log(word)
-  })
+  const downvoteScore = (user) => {
+    let newDefinitions = []
+
+    for (let i = 0; i < word.definitions.length; i++) {
+      const definition = word.definitions[i]
+      if (definition.user === user) {
+        definition.score -= 1
+        newDefinitions.push(definition)
+      } else {
+        newDefinitions.push(definition)
+      }
+    }
+
+    setWord({ definitions: [...newDefinitions], ...word })
+    changeBounty()
+  }
+
+  // bounty.js
+  const [bounty, setBounty] = useState(word.definitions.reduce((accumulator, defn) => accumulator + defn.score, 0) / 1000) 
+
+  const changeBounty = () => {
+    const totalVotes = word.definitions.reduce((accumulator, defn) => accumulator + defn.score, 0)
+    setBounty(totalVotes / 1000)
+  }
 
   return (
     <div className="App">
-      <h1>Fish</h1>
-      { word.definitions.map((defn) => <Definition name={defn.name} score={defn.score} user={defn.user} onClick={upvoteScore} />) }
-
+      <div className="App-header">black box</div>
+      <h1>Fish (asked by @curiouscrustacean)</h1>
+      <p><b>bounty: {bounty} DAI/mo</b></p>
+      <hr />
+        {word.definitions.sort((a, b) => {
+          return b.score - a.score
+        }).map((defn) => (<><Definition name={defn.name} score={defn.score} user={defn.user} upvote={upvoteScore} downvote={downvoteScore} /><hr /></>))}
     </div>
   );
 }
--- v2/src/Definition.js
+++ v2/src/Definition.js
@@ -6,14 +6,12 @@ function Definition(props) {
             <div className="one">
                 <p>{props.name}</p>
             </div>
-            <div>
-                <h3>Score</h3>
-                <h4>{props.score}</h4>
-            </div>
+            <div className="two"></div>
             <div className="three">
-                <span onClick={() => props.onClick(props.user)}>+1</span>
+                <span onClick={() => props.upvote(props.user)}>+</span>
                 <br />
-                <span>-1</span>
+                <h4>{props.score}</h4>
+                <span onClick={() => props.downvote(props.user)}>-</span>
             </div>
         </div>
     )
