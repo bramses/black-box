0ee5b4846c58b0ab49a30de2c9da71de5e47479e
Bram Adams <3282661+bramses@users.noreply.github.com>
2021-04-28 22:47:02 -0400

    router v.01

--- v2/src/App.js
+++ v2/src/App.js
@@ -2,29 +2,68 @@ import './App.css'
 import Word from './Word'
 // import './Ethereum'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
 
   // users -> from json
 
-  const tommy = { address: '0x123' , monthlyPot: '$10' }
-  const becca = { address: '0x456', monthlyPot: '$5' }
+  const tommy = { address: '0x123' , monthlyPot: '$10', bounties: [] } // would this need to be server side?
+  const becca = { address: '0x456', monthlyPot: '$5', bounties: [] }
 
   const words = [{
     name : "Sunfish",
     users: [tommy, becca]
   }]
   
-
   return (
-    <div className="App">
-      <div className="App-header">black box</div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/fish">Fish</Link>
+            </li>
+            <li>
+              <Link to="/word2">Word 2</Link>
+            </li>
+          </ul>
+        </nav>
 
-      {words.map(word => (<div>
-        <Word word={word.name} users={word.users} />
-      </div>))}
-    </div>
-  );
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/fish">
+            <Word name={words[0].name} users={words[0].users} />
+          </Route>
+          {/* <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route> */}
+        </Switch>
+      </div>
+    </Router>
+  )
+
+  // return (
+  //   <div className="App">
+  //     <div className="App-header">black box</div>
+  //     {words.map(word => (<div>
+  //       <Word word={word.name} users={word.users} />
+  //     </div>))}
+  //   </div>
+  // );
 }
 
 export default App;
